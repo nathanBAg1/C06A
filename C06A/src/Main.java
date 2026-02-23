5c7a4d1f82402c96b0afd6fd1b25145b3b472947
nathanBAg1 <nathan.bispoaguiar123@gmail.com>
2026-02-23 18:34:39 -0300

    Exercícios de C06

---
+++ C06A/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z = x + y;
+
+        System.out.println("O valor é: " + (x+y));
+
+        System.out.println("Hello World");
+
+    }
+}
---
+++ ex3_VidaDeEstudante/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main() {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite a NPA: ");
+        int NPA = entrada.nextInt();
+
+        if(NPA >= 60) {
+            System.out.println("Passou!");
+        } else {
+            System.out.println("Nao passou!");
+            System.out.print("Informe o valor de sua NP3: ");
+            int NP3 = entrada.nextInt();
+            int NFA = (NPA + NP3) / 2;
+            if(NFA >= 50) {
+                System.out.println("Passou!");
+            } else {
+                System.out.println("Reprovado!");
+            }
+        }
+
+        entrada.close();
+    }
+}
