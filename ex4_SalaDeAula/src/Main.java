a90f0e8b0cbbb553387e426346b1a0edd39f216d
nathanBAg1 <nathan.bispoaguiar123@gmail.com>
2026-02-24 14:30:09 -0300

    Exercícios de C06

---
+++ ex4_SalaDeAula/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Entre com o número de alunos: ");
+        int numAlunos = entrada.nextInt();
+
+        switch (numAlunos) {
+            case 10,20:
+                System.out.println("Sala I-16");
+            break;
+            case 30:
+                System.out.println("Sala I-22");
+            break;
+            default:
+                System.out.println("Sala não encontrada");
+            break;
+        }
+
+        entrada.close();
+    }
+
+}
